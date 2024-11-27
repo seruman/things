106661c52ad782b9d9b5276ac146b4a384f64855
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-11-27 11:00:06 +0300

    tsquery: add java,python,javascript,typescript languages

--- tsquery/main.go
+++ tsquery/main.go
@@ -12,6 +12,10 @@ import (
 
 	sitter "github.com/smacker/go-tree-sitter"
 	"github.com/smacker/go-tree-sitter/golang"
+	"github.com/smacker/go-tree-sitter/java"
+	"github.com/smacker/go-tree-sitter/javascript"
+	"github.com/smacker/go-tree-sitter/python"
+	"github.com/smacker/go-tree-sitter/typescript/typescript"
 )
 
 func main() {
@@ -79,6 +83,14 @@ func realMain(ctx context.Context, stdin io.Reader, stdout io.Writer, stderr io.
 	switch flagLang {
 	case "go":
 		lang = golang.GetLanguage()
+	case "java":
+		lang = java.GetLanguage()
+	case "python":
+		lang = python.GetLanguage()
+	case "javascript":
+		lang = javascript.GetLanguage()
+	case "typescript":
+		lang = typescript.GetLanguage()
 	default:
 		return fmt.Errorf("unsupported language: %s", flagLang)
 	}
